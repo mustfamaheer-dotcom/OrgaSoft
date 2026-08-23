dfc74ca526451fc8199818973cf3c0c503315996
Moustafa Maher <mustfa.maheer@gmail.com>
2026-08-24 02:55:16 +0300

    Adjust ticker padding for arabic font

--- components/NewsTicker.tsx
+++ components/NewsTicker.tsx
@@ -33,8 +33,8 @@ const NewsTicker: React.FC = () => {
       <div className={`flex items-center h-full whitespace-nowrap w-max hover:[animation-play-state:paused] ${isRTL ? 'animate-seamless-rtl-to-l' : 'animate-seamless-ltr-to-r'}`}>
         {[...Array(16)].map((_, i) => (
           <div key={i} className="flex items-center h-full">
-            <span className="text-xs sm:text-sm font-bold tracking-wider px-8 leading-none pt-0.5">{text}</span>
-            <span className="text-xs opacity-50 px-2 leading-none pt-0.5">•</span>
+            <span className="text-xs sm:text-sm font-bold tracking-wider px-8 pt-1.5">{text}</span>
+            <span className="text-xs opacity-50 px-2 pt-1.5">•</span>
           </div>
         ))}
       </div>
